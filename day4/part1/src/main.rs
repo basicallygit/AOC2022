4dd1eafa1d83c37f3fa6721f1512a18edc5c1ec2
basicallygit <91993321+basicallygit@users.noreply.github.com>
2022-12-04 09:39:46 +0000

    Add files via upload

---
+++ day4/part1/src/main.rs
@@ -0,0 +1,46 @@
+use std::io::{BufRead, BufReader};
+use std::env;
+use std::fs::File;
+
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() <= 1 {
+        eprintln!("No input file sepcified");
+        std::process::exit(1);
+    }
+
+    let file = File::open(&args[1])?;
+    let reader = BufReader::new(file);
+
+    let mut overlap_count: u16 = 0;
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let pairs: Vec<&str> = line.split(',').collect();
+
+        let first_elf = pairs[0];
+        let second_elf = pairs[1];
+    
+        if contains_range(first_elf, second_elf) {
+            overlap_count += 1;
+        }
+    }
+
+    println!("{}", overlap_count);
+    
+    Ok(())
+}
+
+fn contains_range(first: &str, second: &str) -> bool {
+    let first_split: Vec<&str> = first.split('-').collect();
+    let a: u8 = first_split[0].parse().unwrap();
+    let b: u8 = first_split[1].parse().unwrap();
+
+    let second_split: Vec<&str> = second.split('-').collect();
+    let c: u8 = second_split[0].parse().unwrap();
+    let d: u8 = second_split[1].parse().unwrap();
+
+
+    (a <= c && d <= b) || (c <= a && b <= d)
+}
