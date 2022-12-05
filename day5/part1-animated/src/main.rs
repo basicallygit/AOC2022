0bd8f1149b2c5f7e77185dc31e4544e1295597af
basicallygit <91993321+basicallygit@users.noreply.github.com>
2022-12-05 22:40:20 +0000

    Add files via upload

---
+++ day5/part1-animated/src/main.rs
@@ -0,0 +1,82 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::env;
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() <= 1 {
+        eprintln!("No input file specified!");
+        std::process::exit(0);
+    }
+
+    let mut lines = BufReader::new(File::open(&args[1])?)
+        .lines().map(|l| l.unwrap());
+
+    let mut line: String;
+    let mut crates: Vec<Vec<char>> = Vec::new();
+    let mut crate_data = String::new();
+
+
+    //-=-=-=-=-=-=- PARSE CRATES -=-=-=-=-=-=-=-
+    let indexes: Vec<u8> = vec![1, 5, 9, 13, 17, 21, 25, 29, 33];
+    for _ in 0..9 {
+        crates.push(Vec::new());
+    }
+
+    loop {
+        line = lines.next().unwrap();
+        if line.starts_with(' ') {
+            lines.next();
+            break;
+        }
+        crate_data.push_str(&(line +"\n"));
+    }
+    
+    crate_data = crate_data.lines().rev().collect::<Vec<&str>>().join("\n");
+
+    let mut push_crates = |c: char, i: usize| {
+        if c != ' ' {
+            crates[i].push(c);
+        }
+    };
+    
+    for l in crate_data.lines() {
+        for i in 0..9 {
+            push_crates(l.chars().nth(indexes[i] as usize).unwrap(), i);
+        }
+    }
+    //-=-=-=-=-=-=- END PARSE CRATES -=-=-=-=-=-=-=-
+
+    //-=-=-=-=-=-=- PARSE INSTRUCTIONS -=-=-=-=-=-=-=-
+
+    while let Some(l) = lines.next() {
+        print!("\x1B[2J\x1B[1;1H");
+        let split = l.split_whitespace().collect::<Vec<&str>>();
+        let amount = split[1].parse::<usize>().unwrap();
+        let from = split[3].parse::<usize>().unwrap() - 1;
+        let to = split[5].parse::<usize>().unwrap() - 1;
+
+        for _ in 0..amount {
+            let to_append = crates[from].pop().unwrap();
+            crates[to].push(to_append);
+        }
+        for i in 0..9 {
+            println!("{}: {:?}", i+1, crates[i]);
+        }
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
+    //-=-=-=-=-=-=- END PARSE INSTRUCTIONS -=-=-=-=-=-=-=-
+
+    let mut output = String::new();
+    println!("Final state:");
+    for i in 0..9 {
+        println!("{}: {:?}", i+1, crates[i]);
+        //add the character ontop of the crates to the output string
+        output.push(crates[i].pop().unwrap());
+    }
+
+    println!("Output: {}", output);
+
+
+    Ok(())
+}
